5eaa59791f24f88cb4424a83fc9cac19694f42b7
senk8 <revofal@gmail.com>
2020-12-24 13:55:07 +0000

    [UPD]余計なコードを消去しました。

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ pub mod parser;
 pub mod assemble;
 
 use std::env;
-use std::fs::File;
-use std::io::{self,BufReader,BufWriter};
-
 use tokenizer::Tokenizer;
 use parser::expr::expr;
 use assemble::gen;
@@ -16,15 +13,6 @@ fn print_typename<T>(_: T) {
 
 fn main() {
     let arg = env::args().nth(1).unwrap();
-
-    /*
-    let mut buf = "";
-
-    for result in BufReader::new(File::open(arg.as_str())?).lines() {
-        let l = result?;
-        format!("{}{}",buf,l);
-    }
-    */
 
     let mut tokens_iter = Tokenizer::new(arg.as_str()).peekable();
     let node=expr(&mut tokens_iter);
